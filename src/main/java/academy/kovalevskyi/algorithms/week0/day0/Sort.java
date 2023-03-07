db78346a97d48c6f2f3fd07fa78fe4956b8fd01a
Aliaksandr Makaranka <aliaksandrmakaranka@gmail.com>
2023-03-07 13:59:27 +0100

    add create buffer

--- src/main/java/academy/kovalevskyi/algorithms/week0/day0/Sort.java
+++ src/main/java/academy/kovalevskyi/algorithms/week0/day0/Sort.java
@@ -8,32 +8,42 @@ public interface Sort {
     sort(target, T::compareTo);
   }
 
+
+  //it's outside buffer from JCB, interface public always?
+  public static <T> void buffer(T[] target, int i, int j) {
+    T buffer = target[i];
+    target[i] = target[j];
+    target[j] = buffer;
+  }
+
+  //algorithmic complexity O(N^2)
   default <T> void sort(final T[] target, final Comparator<T> comparator) {
-    //todo
+    for (int i = 0; i < target.length; i++) {
+      for (int j = 0; j < target.length - i - 1; j++) {
+        if (comparator.compare(target[j], target[j + 1]) > 0) {
+          buffer(target,j, j + 1);
+        }
+      }
+    }
   }
 
   default <T> T[] createSortedArray(final T[] target, final Comparator<T> comparator) {
-    //todo
-    return null;
+    return null;//todo
   }
 
   default String complexityBest() {
-    //todo
-    return "O(1)";
+    return "N";
   }
 
   default String complexityAverage() {
-    //todo
-    return "O(N^2)";
+    return "N^2";
   }
 
   default String complexityWorst() {
-    //todo
-    return "O(N^2)";
+    return "N^2";
   }
 
   default String spaceComplexityWorst() {
-    //todo
-    return "O(N)";
+    return "N";
   }
 }
