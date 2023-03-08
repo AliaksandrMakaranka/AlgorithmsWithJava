5b47d65288ab1c1a4d99194a93a640b81acbd393
Aliaksandr Makaranka <aliaksandrmakaranka@gmail.com>
2023-03-08 13:32:30 +0100

    tasks HeapSort MergeSort QuickSort

---
+++ src/main/java/academy/kovalevskyi/algorithms/week0/day2/MergeSort.java
@@ -0,0 +1,18 @@
+package academy.kovalevskyi.algorithms.week0.day2;
+
+import academy.kovalevskyi.algorithms.week0.day0.Sort;
+import java.util.Comparator;
+
+public class MergeSort implements Sort {
+
+  @Override
+  public <T> void sort(T[] target, Comparator<T> comparator) {
+    //todo
+  }
+
+  @Override
+  public <T> T[] createSortedArray(T[] target, Comparator<T> comparator) {
+    //todo
+    return null;
+  }
+}
---
+++ src/main/java/academy/kovalevskyi/algorithms/week0/day2/Task.java
@@ -0,0 +1,11 @@
+package academy.kovalevskyi.algorithms.week0.day2;
+
+import java.util.OptionalInt;
+
+public class Task {
+
+  public static OptionalInt findIndex(int[] sortedArray, int target) {
+    //todo
+    return null;
+  }
+}
