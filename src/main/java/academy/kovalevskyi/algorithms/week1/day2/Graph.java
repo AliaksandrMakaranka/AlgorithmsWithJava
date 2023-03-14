133225b83ea2756080571753bc1786f10458f67c
Aliaksandr Makaranka <aliaksandrmakaranka@gmail.com>
2023-03-14 23:39:22 +0100

    add w1d2 Percolation

---
+++ src/main/java/academy/kovalevskyi/algorithms/week1/day2/Graph.java
@@ -0,0 +1,24 @@
+package academy.kovalevskyi.algorithms.week1.day2;
+
+import java.util.Set;
+
+public class Graph {
+
+  private Graph() {
+  }
+
+  public static Graph generateGraph(Set<Node> nodes) {
+    //todo
+    return null;
+  }
+
+  public int countComponents() {
+    //todo
+    return 0;
+  }
+
+  public int getComponentId() {
+    //todo
+    return 0;
+  }
+}
---
+++ src/main/java/academy/kovalevskyi/algorithms/week1/day2/Percolation.java
@@ -0,0 +1,14 @@
+package academy.kovalevskyi.algorithms.week1.day2;
+
+public class Percolation {
+
+  public Percolation(boolean[][] field) {
+    //todo
+  }
+
+  public boolean percolate() {
+    //todo
+    return false;
+  }
+
+}
