6f98c824a810278e54f8b9efe69d6da4fbe21eff
Aliaksandr Makaranka <aliaksandrmakaranka@gmail.com>
2023-03-16 14:35:41 +0100

    add w1d3 Dijkstra

---
+++ src/main/java/academy/kovalevskyi/algorithms/week1/day3/NetworkPathFinder.java
@@ -0,0 +1,9 @@
+package academy.kovalevskyi.algorithms.week1.day3;
+
+public class NetworkPathFinder {
+
+  public static int shortestPath(Node start, Node end) {
+    //todo
+    return 0;
+  }
+}
---
+++ src/main/java/academy/kovalevskyi/algorithms/week1/day3/VirusApocalypse.java
@@ -0,0 +1,9 @@
+package academy.kovalevskyi.algorithms.week1.day3;
+
+public class VirusApocalypse {
+
+  public static int countDays(Integer[][] startingUniverse) {
+    //todo
+    return 0;
+  }
+}
