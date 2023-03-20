3d359ae6c4b600acfa31c283fad6ffdd72f5c29c
Aliaksandr Makaranka <aliaksandrmakaranka@gmail.com>
2023-03-20 18:32:50 +0100

    add w2d0 unblalnced binari tree

---
+++ src/main/java/academy/kovalevskyi/algorithms/week2/day0/IntTreeHelper.java
@@ -0,0 +1,37 @@
+package academy.kovalevskyi.algorithms.week2.day0;
+
+import java.util.List;
+
+public class IntTreeHelper {
+
+  public static IntTreeNode createNode(int value) {
+    //todo
+    return null;
+  }
+
+  public static IntTreeNode addNode(IntTreeNode root, int value) {
+    //todo
+    return null;
+  }
+
+  public static boolean needBalancing(IntTreeNode root) {
+    //todo
+    return false;
+  }
+
+  public static List<Integer> getSortedList(IntTreeNode root) {
+    //todo
+    return null;
+  }
+
+  public static IntTreeNode generateBalanceTree(IntTreeNode root) {
+    //todo
+    return null;
+  }
+
+  public static boolean hasValue(IntTreeNode root, int value) {
+    //todo
+    return false;
+  }
+}
+
---
+++ src/main/java/academy/kovalevskyi/algorithms/week2/day0/IntTreeNode.java
@@ -0,0 +1,12 @@
+package academy.kovalevskyi.algorithms.week2.day0;
+
+public record IntTreeNode(
+    int value,
+    int sum,
+    int count,
+    int maxDepth,
+    int minDepth,
+    IntTreeNode left,
+    IntTreeNode right) {
+
+}
